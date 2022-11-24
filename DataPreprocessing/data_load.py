ffa1c8f027fa79ef5bc51ab5124875296bc50cb1
climbhilltmr <vincentlright@gmail.com>
2022-11-24 17:56:29 +0800

    Update data_load.py

--- DataPreprocessing/data_load.py
+++ DataPreprocessing/data_load.py
@@ -1,2 +1,11 @@
 import pandas as pd 
+import numpy as np
 
+import sys 
+sys.path.append("..") 
+
+DIALYSIS = pd.read_excel('../Dataset/台州中心透析记录.xls')
+
+DOCTORS_ADVICE = pd.read_excel('../Dataset/台州中心医院-HD-透析处方.xls')
+
+LIS_REPORT = pd.read_excel('../Dataset/透析数据--台州市中心医院.xls')
